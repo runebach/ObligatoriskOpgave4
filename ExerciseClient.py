8b1c76fe797582db342674a862a40408c0fd5bd7
runebach <rubaho200@gmail.com>
2023-10-05 17:55:29 +0200

    Asd

--- ExerciseClient.py
+++ ExerciseClient.py
@@ -1,12 +1,32 @@
 from socket import *
+import time
 
-serverName = "localhost"
+# Vi angiver en server host og en server port
+serverName = "127.0.0.1"
 serverPort = 12000
 
+# Vi opretter en TCP client socket og connecter denne
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-print("use format [operand] [operation] [operand] including spaces. "
-      + "Example: 2 + 2")
 
-calculation = input("Input calculation:")
-clientSocket.send(calculation.encode())
+
+while True:
+    print("Choose three operations. Random, Add or Subtract. Choose two numbers.")
+    print("Format as: <Operation num1 num2>. Type Over to quit.")
+
+    # Vi tager et brugerindput og sender dette til serversocketen.
+    calculation = input("Input calculation:")
+
+    if(calculation == "Over"):
+        break
+
+    clientSocket.send(calculation.encode())
+
+    answer = clientSocket.recv(1024)
+
+    print("The answer is:", answer.decode())
+    time.sleep(1)
+
+
+
+
--- ExerciseServer.py
+++ ExerciseServer.py
@@ -1,53 +1,56 @@
 from socket import *
 import threading
+import random
 
+# Vores overordnede funktion, der meget gerne skulle kunne køre igen og igen.
 def HandleClient(connectionSocket, address):
     print(address[0])
     communicate = True
 
+    # Hvis communicate er true, receiver vi et input fra client
     while communicate == True:
-        calculation = connectionSocket.recv(1024).decode().strip()
+        calculation = connectionSocket.recv(1024).decode()
         
         print(calculation)
-        response = "Sorry. Please input a proper message"
+        result = "Sorry. Please input a proper message"
         if calculation == "close":
             communicate = False
-            response = "You have quit the calculator."
+            result = "You have quit the calculator."
 
+        # Vi splitter vores string op. Som default gøres dette efter blank spaces " "
         calculation_list = calculation.split()
-        operand_1 = calculation_list[0]
-        operator = calculation_list[1]
+        operator = calculation_list[0]
+        operand_1 = calculation_list[1]
         operand_2 = calculation_list[2]
 
+        # Vi parser vores operander til floats. Herefter tjekker vi operanden.
         num_1 = float(operand_1)
         num_2 = float(operand_2)
 
-        if operator == "+":
+        if operator == "Random":
+            result = random.randint(num_1, num_2)
+        if operator == "Add":
             result = num_1 + num_2
-        if operator == "-":
+        if operator == "Subtract":
             result = num_1 - num_2
-        if operator == "*":
-            result = num_1 * num_2
-        if operator == "/":
-            if num_2 == 0:
-                result = "Sorry. You cannot divide by 0"
-            if num_2 != 0:
-                result = num_1 / num_2
-        else:
-            result = "Invalid input"
 
+        # Vi sender resultatet tilbage til serveren efter at have konverteret til en streng
         print("Sending result to client")
 
         output = str(result)
         connectionSocket.send(output.encode())
+    # Vi lukker forbindelsen
     connectionSocket.close()
 
+# Vi angiver serverPort og serverHost og danner en ServerSocket. Vi begynder at lytte.
 serverPort = 12000
-serverHost = "127.0.0.2"
+serverHost = "127.0.0.1"
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((serverHost, serverPort))
 serverSocket.listen(1)
 print("Server is ready")
+
+# Vi kører et permanent while loop, der kører HandleClient metoden.
 while True:
     connectionSocket, addr = serverSocket.accept()
     threading.Thread(target=HandleClient, args=(connectionSocket, addr)).start()
